e7440271b2f9cdf09ffb8149f7a7d5e28cd40f92
2jeongfeel <2jeongfeel@gmail.com>
2025-11-16 00:10:08 +0900

    Build for Github Pages

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,11 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+// 레포 이름에 맞게 base 수정
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [vue()],
+  base: '/wedding-invitation/',   // 🔴 레포 이름으로 변경
+  build: {
+    outDir: 'docs'                // GitHub Pages에서 사용할 폴더
+  }
 })
